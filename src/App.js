88b336b45e1c784bff9fac8dc6d64ec53a490fda
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-19 11:41:10 -0600

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import './styles.css';
+
+class App extends Component {
+
+  ComponentDidMount() {
+    console.log('working')
+  }
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
