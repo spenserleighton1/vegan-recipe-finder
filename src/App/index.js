d6b3877928c5f443d51e10ec8433b2e38c498f69
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-19 14:41:24 -0600

    Add redux to App

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './styles.css';
+import { fetchRecipes } from '../helper/apiCalls';
+import { apiKey } from '../helper/apiKey';
+import { cleanData } from '../helper/dataCleaner';
+import { addRecipes } from '../actions';
+import RecipesContainer from '../RecipesContainer';
 
 class App extends Component {
-  componentDidMount() {
-    // console.log('working')
+
+async componentDidMount() {
+    const results = await fetchRecipes(apiKey);
+    const recipes = await cleanData(results.recipes);
+    this.props.addRecipes(recipes)
   }
 
   render() {
     return (
       <div className="App">
+      <RecipesContainer />
       </div>
     );
   }
 }
 
-export default App;
+export const mapStateToProps = (state) => ({
+  recipes: state.recipes
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  addRecipes: (recipes)=> dispatch(addRecipes(recipes))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
