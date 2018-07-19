0870868157adb16f3ac77b95221804382298adbb
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-19 15:16:13 -0600

    Create and import styles.css

--- src/RecipeCard/index.js
+++ src/RecipeCard/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './styles.css'
 
 const RecipeCard = ({recipeTitle, recipeLink, recipeImage }) => {
   return (
-    <div>
-      <h2>{ recipeTitle }</h2>
-      <img src={ recipeImage } alt={ recipeTitle } />
-      <a href={ recipeLink }>View Recipe</a>
+    <div className='recipe-card'>
+      <h3 className='recipe-title'>{ recipeTitle }</h3>
+      <img className='recipe-img' src={ recipeImage } alt={ recipeTitle } />
+      <a className='recipe-link' href={ recipeLink }>View Recipe</a>
     </div>
     )
 }
