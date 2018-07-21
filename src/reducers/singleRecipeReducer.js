8e8a2cf9cd3e4dcbce99203e29e4570dd96152cf
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-21 13:38:27 -0600

    Display single recipe on page

--- src/reducers/singleRecipeReducer.js
+++ src/reducers/singleRecipeReducer.js
@@ -1,4 +1,4 @@
-export const singleRecipeReducer = (state = [], action) => {
+export const singleRecipeReducer = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_SINGLE_RECIPE':
       return action.recipe;
