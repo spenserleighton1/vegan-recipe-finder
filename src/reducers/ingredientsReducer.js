2647ae447c975928a9cdedc047806c50b3288738
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-21 12:00:14 -0600

    Delete ingredients reducer file

--- src/reducers/ingredientsReducer.js
+++
@@ -1,8 +0,0 @@
-export const ingredientsReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'SEARCH_BY_INGREDIENT':
-      return [...state, action.ingredients];
-    default:
-      return state;  
-  }
-}
