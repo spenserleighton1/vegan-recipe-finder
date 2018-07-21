2d6197f1990ef520c608bb3e473fa6058495a2f9
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-21 11:15:01 -0600

    Add apiCall for single recipe

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -1,5 +1,12 @@
-export const fetchRecipes = async (key) => {
-  const url = `https://food2fork.com/api/search?key=${key}&q=vegan`;
+export const fetchRecipes = async (key,ingredient) => {
+  const url = `https://food2fork.com/api/search?key=${key}&q=vegan,${ingredient}`;
+  const response = await fetch(url);
+  const results = await response.json();
+  return results;
+}
+
+export const fetchSingleRecipe = async (key,rId) => {
+  const url = `http://food2fork.com/api/get?key=${key}&rId=${rId}`
   const response = await fetch(url);
   const results = await response.json();
   return results;
