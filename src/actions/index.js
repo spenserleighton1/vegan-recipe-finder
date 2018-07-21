84e81a1c6fc286390d5de75e63775d8d490efb83
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-21 11:16:18 -0600

    Add ingredients action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const addRecipes = (recipes) => ({
   type: 'ADD_RECIPES',
   recipes
 })
+
+export const searchByIngredient = (ingredients) => ({
+  type: 'SEARCH_BY_INGREDIENT', 
+  ingredients
+})
