e0b451a3689a951bd047c17c1e616a6979d2d9a9
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-22 13:45:06 -0600

    Add tests to App

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './';
+import { shallow } from 'enzyme';
+import App, { mapDispatchToProps } from './';
+import { addRecipes } from '../actions';
 
-it('renders without crashing', () => {
+describe('App', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />)
 
-});
+    expect(wrapper).toMatchSnapshot()
+  })
+  describe('mapDispatchToProps', () => {
+    it('should return propsObject', () => {
+      const mockAddRecipes = jest.fn();
+      const mockDispatch = jest.fn();
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      const actionToDispatch = addRecipes([{title: 'food'}])
+
+      mappedProps.addRecipes([{title: 'food'}])
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
+  })
+})
