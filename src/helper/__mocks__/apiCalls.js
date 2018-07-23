8462086e34c5e3e2fa109b2e7491de5170679a47
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-23 15:04:09 -0600

    Add api mocks to mocks folder

--- src/helper/__mocks__/apiCalls.js
+++ src/helper/__mocks__/apiCalls.js
@@ -1,3 +1,5 @@
 export const fetchRecipes = jest.fn().mockImplementation(() => ({
   recipes: [{title: 'a food'}, {title: 'more a food'}, {title: 'even more a food'}]
 }));
+
+export const fetchSingleRecipe = jest.fn()
