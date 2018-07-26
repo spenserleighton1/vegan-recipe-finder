150c31ccee3c7d0340f3cfb2de550d83e2dc5f24
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-25 20:48:27 -0600

    Add single loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import { recipeReducer } from './recipeReducer';
-import { singleRecipeReducer } from './singleRecipeReducer'
+import { singleRecipeReducer } from './singleRecipeReducer';
+import { isLoadingReducer } from './isLoadingReducer';
+import { singleIsLoadingReducer } from './singleIsLoadingReducer'
 
 const rootReducer = combineReducers({
   recipes: recipeReducer,
-  singleRecipe: singleRecipeReducer
+  singleRecipe: singleRecipeReducer,
+  loading: isLoadingReducer,
+  singleLoading: singleIsLoadingReducer
 });
 
 export default rootReducer;
