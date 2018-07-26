650cc784b430bf28a5cac02c0cceff1fabc41e81
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-25 20:47:55 -0600

    Take out landing page

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const addSingleRecipe = (recipe) => ({
   type: 'ADD_SINGLE_RECIPE', 
   recipe
 })
+
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool
+})
+
+export const singleIsLoading = (bool) => ({
+  type: 'SINGLE_IS_LOADING',
+  singleIsLoading: bool
+})
