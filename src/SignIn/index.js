6e97f94090928deec8d412ac4f286fdf29fa74b2
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-28 09:57:19 -0600

    Refactor firebase

--- src/SignIn/index.js
+++ src/SignIn/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { SignUpLink } from '../SignUp';
-
 import { auth } from '../firebase';
 import './styles.css'
 
