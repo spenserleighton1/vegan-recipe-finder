6856a1bc9b3f65e04e972570047bc2c2c6888875
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-28 10:01:11 -0600

    Add saveRecipe reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux';
 import { recipeReducer } from './recipeReducer';
 import { singleRecipeReducer } from './singleRecipeReducer';
 import { isLoadingReducer } from './isLoadingReducer';
-import { singleIsLoadingReducer } from './singleIsLoadingReducer'
+import { saveRecipeReducer } from './saveRecipeReducer';
 
 const rootReducer = combineReducers({
   recipes: recipeReducer,
   singleRecipe: singleRecipeReducer,
   loading: isLoadingReducer,
-  singleLoading: singleIsLoadingReducer
+  savedRecipes: saveRecipeReducer
 });
 
 export default rootReducer;
