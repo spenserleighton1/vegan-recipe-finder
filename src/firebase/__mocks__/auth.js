b7e1158d28e9fd578b616f3c91114b1c192d22a7
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-29 09:34:10 -0600

    Make attempt at mocking firebase

---
+++ src/firebase/__mocks__/auth.js
@@ -0,0 +1,6 @@
+// Sign Up
+export const doCreateUserWithEmailAndPassword = jest.fn();
+// Sign In
+export const doSignInWithEmailAndPassword = jest.fn();
+// Sign out
+export const doSignOut = jest.fn();
