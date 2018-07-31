e5cd5ad01e15cb741e1bd9d99458fe90872ac902
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-30 21:36:39 -0600

    Start that sick linting

--- src/reducers/tests/savedRecipeReducer.test.js
+++ src/reducers/tests/savedRecipeReducer.test.js
@@ -12,25 +12,25 @@ describe('savedRecipesReducer', () => {
 
   it('should return an array of  saved recipes', () => {
     const recipes = [{recipeTitle: 'best food ever',
-                      recipeImage: 'cute picture',
-                      recipeLink: 'bestlink.com',
-                      id: 1},
-                      {
-                      recipeTitle: 'another good food',
-                      recipeImage: 'cuter picture',
-                      recipeLink: 'evenbetterlink.com',
-                      id: 1
-                      }];
+      recipeImage: 'cute picture',
+      recipeLink: 'bestlink.com',
+      id: 1},
+    {
+      recipeTitle: 'another good food',
+      recipeImage: 'cuter picture',
+      recipeLink: 'evenbetterlink.com',
+      id: 1
+    }];
 
     const expected = [[{"id": 1, 
-                        "recipeImage": "cute picture", 
-                        "recipeLink": "bestlink.com", 
-                        "recipeTitle": "best food ever"}, 
-                       {"id": 1, 
-                        "recipeImage": "cuter picture", 
-                        "recipeLink": "evenbetterlink.com", 
-                        "recipeTitle": "another good food"}]]
-    const results = savedRecipesReducer(undefined, actions.addSavedRecipes(recipes))
+      "recipeImage": "cute picture", 
+      "recipeLink": "bestlink.com", 
+      "recipeTitle": "best food ever"}, 
+    {"id": 1, 
+      "recipeImage": "cuter picture", 
+      "recipeLink": "evenbetterlink.com", 
+      "recipeTitle": "another good food"}]];
+    const results = savedRecipesReducer(undefined, actions.addSavedRecipes(recipes));
 
     expect(results).toEqual(expected);
   });
